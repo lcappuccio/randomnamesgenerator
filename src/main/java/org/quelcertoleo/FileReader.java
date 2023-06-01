6ff19a3b4c87608432f1ffb868a53f77f19418a0
Leonardo <lcappuccio@some-random-email.org>
2023-06-01 22:53:22 +0200

    add FileReader

---
+++ src/main/java/org/quelcertoleo/FileReader.java
@@ -0,0 +1,12 @@
+package org.quelcertoleo;
+
+import java.util.List;
+
+public class FileReader {
+
+    private FileReader() {}
+
+    public static List<String> getNamesFromFile(final String fileName) {
+        throw new UnsupportedOperationException();
+    }
+}
